1165d07f602bc8626bf20a38c747fe594743faf2
Hussain Ali <ali.415code@gmail.com>
2021-02-26 14:43:05 -0800

    Added find by name method

--- src/main/java/com/buildweek/usemytechstuff/repositories/UserRepository.java
+++ src/main/java/com/buildweek/usemytechstuff/repositories/UserRepository.java
@@ -5,4 +5,11 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long>
 {
+    /**
+     * Find a user based off over username
+     *
+     * @param username the name (String) of user you seek
+     * @return the first user object with the name you seek
+     */
+    User findByUsername(String username);
 }
