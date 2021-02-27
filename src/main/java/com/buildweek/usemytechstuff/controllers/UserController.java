622907bf88b591a11ac0959d96e82df2d550333f
Hussain Ali <ali.415code@gmail.com>
2021-02-27 11:19:10 -0800

    Add new user POST Request complete

--- src/main/java/com/buildweek/usemytechstuff/controllers/UserController.java
+++ src/main/java/com/buildweek/usemytechstuff/controllers/UserController.java
@@ -3,14 +3,17 @@ package com.buildweek.usemytechstuff.controllers;
 import com.buildweek.usemytechstuff.models.User;
 import com.buildweek.usemytechstuff.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 @Controller
@@ -62,13 +65,38 @@ public class UserController
      * @return JSON object of the user you seek
      * @see UserService#findByName(String) UserService.findByName(String)
      */
-    @GetMapping(value = "/user/name/{userName}",
-        produces = "application/json")
+    @GetMapping(value = "/user/name/{userName}", produces = "application/json")
     public ResponseEntity<?> getUserByName(@PathVariable String userName)
     {
         User u = userService.findByName(userName);
         return new ResponseEntity<>(u, HttpStatus.OK);
     }
 
-    
+    /**
+     * Given a complete User Object, create a new User record and accompanying useremail records
+     * and user role records.
+     * <br> Example: <a href="http://localhost:2019/users/user">http://localhost:2019/users/user</a>
+     *
+     * @param newuser A complete new user to add including emails and roles.
+     *                roles must already exist.
+     * @return A location header with the URI to the newly created user and a status of CREATED
+     * @throws URISyntaxException Exception if something does not work in creating the location header
+     * @see UserService#save(User) UserService.save(User)
+     */
+    @PostMapping(value = "/user", consumes = "application/json")
+    public ResponseEntity<?> addNewUser(@Valid @RequestBody User newuser) throws URISyntaxException
+    {
+        newuser.setUserid(0);
+        newuser = userService.save(newuser);
+
+        // set the location header for the newly created resource
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newUserURI = ServletUriComponentsBuilder.fromCurrentRequest()
+            .path("{/userid}")
+            .buildAndExpand(newuser.getUserid())
+            .toUri();
+        responseHeaders.setLocation(newUserURI);
+
+        return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
+    }
 }
